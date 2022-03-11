9d39ce68f2b48a13afadb54542a8bc1c2254da09
KenjxJJ <joelkiggwe94@gmail.com>
2022-03-11 13:11:06 -0800

    Add check last digit file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include <string.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	char str[50];
+	int last_digit;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	
+	last_digit = n%10;
+	  
+	if (last_digit > 5)
+	  strcpy(str, "and is greater than 5");
+	else if(last_digit == 0)
+	  strcpy(str,"and is 0");
+	else
+	  strcpy(str,"and is less than 6 and not 0");
+	
+	printf("Last digit of %d is %d and %s\n", n, last_digit, str );
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+## C-Programming
+
+# If, if else and while loops
