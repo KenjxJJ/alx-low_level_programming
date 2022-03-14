3feb43cca08b033dc0f4e801290802788489885e
KenjxJJ <joelkiggwe94@gmail.com>
2022-03-14 14:20:34 -0700

    Fix file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main function - First function to be executed
-* @void: Null object.
-*
-* Description: The first function to be called in the program.
-* The function prints string value with an escape character.
-* Return: The function gives a null object.
-*/
+ * main - First function to be executed
+ * @void: Null object.
+ *
+ * Description: The first function to be called in the program.
+ * It prints prints string value with an escape character.
+ * Return: The function gives a null object.
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle.");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
