ed5d9eedce1368d72b4244d7e6323ad1a0bf99ca
KenjxJJ <kenjxjj@gmail.com>
2022-03-18 21:46:32 +0300

    Add a file with a function to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - Multiplies two integers
+ *@c - int number 1
+ *@b - int number 2
+ *
+ * Return: Product of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,3 +7,9 @@ int _isupper(int c);
  * function _isdigit
  */
 int _isdigit(int c);
+
+/**
+ * function mul
+ *
+ */
+int mul(int a, int b);
