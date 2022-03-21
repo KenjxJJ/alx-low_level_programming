603106c0f24e457277648ebcf5f7e5a5893370ae
KenjxJJ <kenjxjj@gmail.com>
2022-03-21 22:16:22 +0300

    Add a file that swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - Swaps two integers
+ * @a - First Integer
+ * @b - Second Integer
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	/* Temporal holder of one value */
+	int *p;
+
+	*p = *b;
+	*b = *a;
+	*a = *p;
+}
