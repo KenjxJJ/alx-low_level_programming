f487598850d5837f41c1747931bcc5bd54a33bbe
KenjxJJ <kenjxjj@gmail.com>
2022-03-22 00:40:20 +0300

    Fix 3-puts file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_ puts - Function that prints a string followed by a new line
+ * _puts - Function that prints a string followed by a new line
  * @s: char array of string pointer
  *
  * Return: Always 0
@@ -11,10 +11,11 @@ void _puts(char *s)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		int su;
+
+		su = _putchar(s[i]);
 		i++;
 	}
 }
