c6869f854fb41ebb4f03b4c5497a764de84fc722
KenjxJJ <kenjxjj@gmail.com>
2022-03-22 00:09:44 +0300

    Add a file that prints a string on a line.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/**
+ *_ puts - Function that prints a string followed by a new line
+ * @s: char array of string pointer
+ *
+ * Return: Always 0
+ */
+void _puts(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
