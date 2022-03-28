03fcc4a788dbb3703570828f6ce1cec4ff15db4d
KenjxJJ <kenjxjj@gmail.com>
2022-03-29 00:31:30 +0300

    Add file to print chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,8 +5,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 
-/*Use of putchar */
+/* Use of putchar */
 int _putchar(char c);
 
-/*Function that locates a string */
+/* Function that locates a string */
 char *_strstr(char *haystack, char *needle);
+
+/* Functiont to print chessboard */
+void print_chessboard(char (*a)[8]);
